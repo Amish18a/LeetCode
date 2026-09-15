d34c365164e11cb63ec99059bf2a3ee316895201
Amish <amish1818a@gmail.com>
2026-09-15 22:12:07 +0530

    Time: 91 ms (65.87%) | Memory: 25.3 MB (94.50%) - LeetSync

---
+++ 403-frog-jump/frog-jump.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canCross(vector<int>& stones) {
+    int n = stones.size();
+    if (n < 2) return true;
+    if (stones[1] != 1) return false;
+
+    vector<vector<bool>> dp(n, vector<bool>(n + 1, false));
+    dp[0][0] = true;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            int k = stones[i] - stones[j];
+            if (k > n) continue;
+            for (int prev_k : {k - 1, k, k + 1}) {
+                if (prev_k >= 0 && prev_k <= n && dp[j][prev_k]) {
+                    dp[i][k] = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    for (int k = 0; k <= n; k++)
+        if (dp[n - 1][k]) return true;
+    return false;
+}         
+};
